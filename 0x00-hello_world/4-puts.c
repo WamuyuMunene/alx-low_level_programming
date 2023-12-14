abf340f54f5d435052fdcf62e98a1206da02cd4e
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-14 08:38:24 -0800

    betty correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /*
- * main - code block
  * int main(void) - returns int (0) if true
  * puts - used to print string
 */
-int main(void) /*main - code block*/
 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
