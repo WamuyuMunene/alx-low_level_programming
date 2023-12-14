1491946e9b224b753d96d9f5b07a59bda99a03ec
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-14 10:45:53 -0800

    size of to get datatype sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Return: 0
+ *
+ * using size of to get size of various data types
+*/
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of float %zu byte(s)\n", sizeof(float));
+	printf("Size of long int %zu byte(s)\n", sizeof(long int));
+	printf("Size of long long int %zu byte(s)\n", sizeof(long long int));
+	return (0);
+}	
