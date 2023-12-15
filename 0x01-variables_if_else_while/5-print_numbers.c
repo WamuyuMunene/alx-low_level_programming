5a6b47741e22ff3dd4521972c222a12847905a48
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-15 06:41:41 -0800

    prints number 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints single digits of base 10
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	if (a <= 9)
+		printf("%d\n", a);
+	a++;
+	return (0);
+}
