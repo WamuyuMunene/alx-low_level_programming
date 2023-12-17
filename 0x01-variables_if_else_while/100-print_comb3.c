5485762874633d3d1e5d923fa7484f0548aae6ca
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-17 02:50:16 -0800

    print combinations of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,16 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 9 ;a++)
+	for (a = '0'; a <= '9' ;a++)
 	{
-		for (b = 0; b <=9; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 		if (a != b)
 		{
 			putchar(a);
 			putchar(b);
 		}
-		if (a <= 9 && b <=9)
+		if (a <= '9' && b <= '9')
 		{
 		putchar(44);
 		putchar(32);
