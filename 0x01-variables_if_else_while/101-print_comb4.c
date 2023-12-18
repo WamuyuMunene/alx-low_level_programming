b670cb2ea0852c8c2d8ea11a96ef3a103f9a8519
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-18 05:37:46 -0800

    print combination of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * print all possible combinations of three numbers
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9' ; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+			if (a != b != c && a < b < c)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a < '7' || b < '8' || c < '9')
+				{
+					putchar(44);
+					putchar(32);
+					}}}}}
+					putchar('\n');
+					return (0);
+}
