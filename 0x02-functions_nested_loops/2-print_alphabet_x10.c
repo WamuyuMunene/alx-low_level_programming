5226fb0e80e35d4574eb52ed049739c5d385a3a1
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-21 06:12:22 -0800

    prints alphabet 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 
 	for (times = 0; times <= 9; times++)
 	{
-		for ( alpha = 'a'; alpha <= 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
