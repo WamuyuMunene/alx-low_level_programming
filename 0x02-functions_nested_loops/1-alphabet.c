a8cd46806846da51368ade2d0363288bc51484fa
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-21 06:00:47 -0800

    print lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * print_alphabet - prints alphabet
  *
  * prints alphabet in lowercase using _putchar
  *
@@ -12,7 +12,7 @@ void print_alphabet(void)
 {
 	int alpha;
 
-	for (alpha = 'a';alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
