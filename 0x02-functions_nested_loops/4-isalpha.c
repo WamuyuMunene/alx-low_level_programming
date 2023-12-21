7cd9b7bf034c774f79dad6fdaba303eab67c812c
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-21 06:32:16 -0800

    checks if alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if it's an alphabet
+ *
+ * Return: 0 or 1
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
