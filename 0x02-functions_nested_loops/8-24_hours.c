2fd4e06e854fecf5457224ace2e6be5ac22e4ef7
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-22 00:03:17 -0800

    print jack bauer time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints 24 series time
+ *
+ * Return: void
+*/
+
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour % 100) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min % 100) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}}
+}
