4ba1bbf387bb1b4f872823f721bd9eaedf2e3dc6
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-22 01:04:28 -0800

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print single digit integers
+ *
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
