3f1cdf892bb59f357fc1758c940c1108978a9c48
WamuyuMunene <wamuyumunene98@gmail.com>
2023-12-21 23:18:10 -0800

    return last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of number
+ *
+ * @a: parameter
+ *
+ * Return: value of last digit
+*/
+
+int print_last_digit(int a)
+{
+	int ones = a % 10;
+
+	if (ones < 0)
+	{
+		ones *= -1;
+	}
+	_putchar(ones);
+	return (ones);
+}
