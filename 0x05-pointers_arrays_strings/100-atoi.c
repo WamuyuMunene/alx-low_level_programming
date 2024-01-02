6cdcb1d7dd3a1f2c40c1014133e7795973b0a9c1
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 08:44:05 -0800

    convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,9 @@ int _atoi(char *s)
 {
 	int value = atoi(s);
 	
-	if (value >= 48 || value <= 57)
+	while (value >= 48 || value <= 57)
+	{
 		_putchar(value);
-	else
-		return (0);
+	}
+	return (0);
 }
