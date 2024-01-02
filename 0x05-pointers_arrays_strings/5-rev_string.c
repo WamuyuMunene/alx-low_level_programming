4c450bae1f2bf1867672998bd1151ce4fba7c54c
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 05:09:00 -0800

    reverses string in array

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * rev_string - reverses string
@@ -10,7 +11,7 @@
 
 void rev_string(char *s)
 {
-	int length = _strlen(s);
+	int length = strlen(s);
 	int first = 0;
 	int last = length - 1;
 
