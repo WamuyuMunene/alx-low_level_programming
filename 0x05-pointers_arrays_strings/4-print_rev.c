04049a37cecb13d75b505266b103003467df4bcf
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 04:43:07 -0800

    prit string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,8 +14,8 @@ void print_rev(char *s)
 
 	for (index = 0; s[index] != '\0'; index++);
 	for (index--; index >= 0; index--)
-		{
-			_putchar(s[index]);
-		}
+	{
+		_putchar(s[index]);
+	}
 		_putchar('\n');
 }
