63c0314d20108487919b338c2ea5ede1a22ed70f
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 06:28:45 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copies string
+ * @dest: string
+ * @src: string
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
