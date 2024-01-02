70a185fc7b732def6c23089b75130b2bcfdac761
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 05:07:47 -0800

    reverses string in array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length = _strlen(s);
+	int first = 0;
+	int last = length - 1;
+
+	if (first < last)
+	{
+		char reverse = s[first];
+		s[first] = s[last];
+		s[last] = reverse;
+
+		first++;
+		last--;
+	}
+}
