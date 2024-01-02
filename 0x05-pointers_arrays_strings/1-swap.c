5d05343582212eefbe007b2399dbc74e150a6a47
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 01:48:37 -0800

    swap values of integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int change = *a;
+	int *a = *b;
+	int *b = change;
+}
