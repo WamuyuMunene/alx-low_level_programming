32bb803d4fe32acfbaa7081564691f631decad7c
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-02 01:23:46 -0800

    updates value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take pointer to int as parameter ad changes val
+ *
+ * *n: pointer
+ *
+ * Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	int *n = 98;
+}
