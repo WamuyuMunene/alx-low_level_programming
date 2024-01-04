485d7a9474a35a868a584f99542290d38faacf56
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-04 04:35:45 -0800

    compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares strings
+ *
+ * @s1: char
+ * @s2: char
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+
+	return (0);
+}
