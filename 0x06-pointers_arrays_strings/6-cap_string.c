cf91ea45d2cb9827accc93611c0ef71b7e603ad7
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-04 05:48:12 -0800

    capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * *cap_string - capitalize each word of string
+ *
+ * @str: string
+ *
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (
+			str[index] - 1 == ',' ||
+			str[index] - 1 == ';' ||
+			str[index] - 1 == '.' ||
+			str[index] - 1 == '!' ||
+			str[index] - 1 == '?' ||
+			str[index] - 1 == '"' ||
+			str[index] - 1 == '(' ||
+			str[index] - 1 == ')' ||
+			str[index] - 1 == '{' ||
+			str[index] - 1 == '}')
+		{
+			str[index] = toupper((unsigned char)str[index]);
+			index++;
+		}
+		return (str);
+	}
+}
