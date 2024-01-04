a084110f077ae02955f96ee0d1b4b366d3b39588
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-04 05:11:26 -0800

    reverse order of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,13 +14,14 @@ void reverse_array(int *a, int n)
 	int first = 0;
 	int last = n - 1;
 
-	while (first < last);
+	while (first < last)
 	{
-	int value = a[first];
-	a[first] = a[last];
-	a[last] = value;
-	
-	first++;
-	last--;
+		int value = a[first];
+
+		a[first] = a[last];
+		a[last] = value;
+
+		first++;
+		last--;
 	}
 }
