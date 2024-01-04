7b3a4983d58260300a60acf1e581383c2200769a
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-04 06:15:33 -0800

    capitalize each word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,7 +27,7 @@ char *cap_string(char *str)
 			str[index - 1] == '}' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == '\t' ||
-			str[index - 1] == ' ')
+			str[index - 1] == 32)
 		{
 			str[index] = toupper((unsigned char)str[index]);
 		}
