f8cfd071adb5903c3572e9582766ff256c6aef5d
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-08 04:07:57 -0800

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * prints chessboard
+ *
+ * @a: array
+ * @[8]: index
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int down;
+	int accross;
+
+	for (down = 0; down < 8; down++)
+	{
+		for (accross = 0; accross < 8; accross++)
+		{
+			_putchar(a[down][accross]);
+		}
+		_putchar('\n');
+	}
+}
