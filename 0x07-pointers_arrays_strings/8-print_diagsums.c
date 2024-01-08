b919bcf137cc73ddfd80367ab75c41c688c54b01
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-08 04:41:16 -0800

    set value of pointer to char

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prin sum of t diagonals of square matrix
+ *
+ * @a: int
+ * @size: int
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+
