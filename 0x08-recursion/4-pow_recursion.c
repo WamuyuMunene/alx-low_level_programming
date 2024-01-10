6fddae54edeb38ad59130ea69dc095932bf527c3
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-10 04:07:34 -0800

    exponentiate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,9 +15,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if (y == 0 || y == 1)
+	else if (y == 0)
 	{
 		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
 	}
 	else
 	{
