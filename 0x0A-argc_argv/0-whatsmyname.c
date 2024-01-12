f7ea70a0803412e9b349ec6cfa523ab462b4fb34
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-12 04:20:01 -0800

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print program name\n
+ *
+ * argc: int
+ * argv: char
+ *
+ * Return: int
+ */
+
+int main(int argc, char argv[])
+{
+	(void) argc;
+
+	_putchar(argv[0]);
+	_putchar('\n');
+
+	return (0);
+}
