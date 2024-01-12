edab143ae96a4ac8650df47a20d14bf3e29881a4
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-12 03:10:38 -0800

    corrections

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c 
-gcc -Wall -pedantic -Werror -Wextra -c ../0x05-pointers_arrays_strings/*.c
-gcc -Wall -pedantic -Werror -Wextra -c ../0x06-pointers_arrays_strings/*.c
-gcc -Wall -pedantic -Werror -Wextra -c ../0x07-pointers_arrays_strings/*.c
-gcc -Wall -pedantic -Werror -Wextra -c ../0x02-functions_nested_loops/*.c
-gcc -Wall -pedantic -Werror -Wextra -c ../0x04-more_functions_nested_loops/*.c
-gcc -Wall -pedantic -Werror -Wextra -c ../0x01-variables_if_else_while/*.c
-
-ar rc liball.a *.o \
-../0x05-pointers_arrays_strings/*.o \
-../0x06-pointers_arrays_strings/*.o \
-../0x07-pointers_arrays_strings/*.o \
-../0x02-functions_nested_loops/*.o \
-../0x04-more_functions_nested_loops/*.o \
-../0x01-variables_if_else_while/*.o
+gcc -c ./*.c
+ar -rc liball.a ./*.o
