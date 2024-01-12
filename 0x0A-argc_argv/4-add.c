d7c53f116aa8549e4c49334a0a4b33ef1ca44ce7
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-12 06:19:37 -0800

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
-	if (argc == 0)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 
 		while (*value)
 		{
-			if (*value < 0 || *value > 9)
+			if (*value < '0' || *value > '9')
 			{
 				printf("Error\n");
 
