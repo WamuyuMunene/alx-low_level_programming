bcf3f7b2b803f051c735789df1b76b7c255da9fc
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-12 06:16:11 -0800

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		char *value = argv[i];
+
+		while (*value)
+		{
+			if (*value < 0 || *value > 9)
+			{
+				printf("Error\n");
+
+				return (1);
+			}
+			value++;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
