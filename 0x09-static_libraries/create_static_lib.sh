fc8a340965e743ef6f069f00fe6f7bc035f21b83
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-12 02:16:50 -0800

    script for creating static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,8 @@
-gcc -Wall -pedantic -Werror -Wextra -c *.c
+gcc -Wall -pedantic -Werror -Wextra -c alx-low_level_programming
+/0x05-pointers_arrays_strings/*.c alx-low_level_programming
+/0x06-pointers_arrays_strings/*.c alx-low_level_programming
+/0x07-pointers_arrays_strings/*.c alx-low_level_programming
+/0x02-functions_nested_loops/*c alx-low_level_programming
+/0x04-more_functions_nested_loops/*.c alx-low_level_programming
+/0x01-variables_if_else_while/*.c
 ar rc liball.a *.o
