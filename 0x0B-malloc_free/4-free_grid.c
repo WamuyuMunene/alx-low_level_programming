56dd702d6517128ba6272109f67a86da1745ead1
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-15 06:37:50 -0800

    free grid from question3

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees 2d grid from above quiestion
+ *
+ * @**grid: pointer
+ * @height: int
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int row;
+
+	for (row = 0; row < height; row++)
+	{
+		free(grid[row]);
+
+		free(grid);
+	}
+}
