536cc90f9e2dcd43d66558e976ef4c1a00499f96
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-15 04:56:50 -0800

    concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,12 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *str;
+
 	if (s1 == NULL || s2 == NULL)
 	{
 		return (NULL);
 	}
-
-	char *str;
 
 	str = (char *) malloc((strlen(s1) + strlen(s2)) + 1);
 
