defe4272bd3307bd8c45f64c58a6f590e901b0e6
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-18 05:52:33 -0800

    allocates mem using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ *
+ * @b: int
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
