29659e511f62fe136175dd5dc7799855a90be314
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-18 08:21:35 -0800

    allocates mem to array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - allocates memory for array
+ *
+ * @nmemb: int
+ * @size: int
+ *
+ * Return: pointer or null
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *array;
+	int i;
+
+	if (nmemb <= 0 || size <= 0)
+        {
+                return (NULL);
+        }
+
+	array = (int *)calloc(nmemb, size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = 0;
+	}
+
+	return (array);
+}
