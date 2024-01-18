bbb294418b746e219033444bacdda87a853bebf3
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-18 09:43:57 -0800

    realloc memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - reallocates mem using malloc and free
+ *
+ * @ptr: pointer
+ * @old_size: int
+ * @new_size: int
+ *
+ * Return: ptr or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == 0)
+	{
+		free (ptr);
+		return (NULL);
+	}
+
+	new_ptr = realloc(ptr, new_size);
+
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	return (new_ptr);
+}
