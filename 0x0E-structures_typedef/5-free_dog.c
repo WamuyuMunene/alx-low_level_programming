c8c9f0d48f6341e00df2709fe9099a5c30723abc
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-22 05:54:26 -0800

    free mem for struct and elements

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free struct and elements
+ *
+ * @d: struct
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
