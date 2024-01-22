fcf738c01306282f5b2938049f7629a63518c8de
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-22 02:58:05 -0800

    print filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of file it's compiled from
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
