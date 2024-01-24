a49de1a1b7573954498931ca154176518f404d4f
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-24 11:29:13 -0800

    function that prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - function that prints a name
@@ -11,5 +12,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+	{
+		return;
+	}
 	f(name);
 }
