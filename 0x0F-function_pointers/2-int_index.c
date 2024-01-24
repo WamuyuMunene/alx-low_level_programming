5f2b34e0d5a4d7e3f341cf947b6d922f4a91fa37
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-24 11:55:58 -0800

    searches for int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: pointer
+ * @size: int
+ * @cmp: pointer
+ *
+ * Return: int 0/1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] > 0))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
