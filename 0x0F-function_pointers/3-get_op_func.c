7f847c6a1f480d9a63fe8c869544df02a617900f
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-25 11:10:44 -0800

    operations function file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,6 @@
+#include "calc.h"
+
+/**
+ * get_op_func - obtains correct function
+ *
+ * @s: 
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,83 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - returns sum
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return sutraction value
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: -
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return mul valu
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: *
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns division value
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - return modulo value
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: mod value
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
