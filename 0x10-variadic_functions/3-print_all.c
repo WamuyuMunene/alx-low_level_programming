6ce8607330eb1f3076e7dc63e85f69a68c93b426
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-25 09:36:41 -0800

    print everything of varying data types

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - prints anything
+ *
+ * @format: argument list
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list array;
+
+	va_start(array, format);
+	vprintf(format, array);
+
+	va_end(array);
+}
