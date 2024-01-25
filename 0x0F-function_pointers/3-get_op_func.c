287236dc54d875a1d33a4da4c2aff1834f69a153
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-25 11:35:09 -0800

    operator caller file

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,33 @@
 #include "calc.h"
+#include <stdlib.h>
 
 /**
  * get_op_func - obtains correct function
  *
- * @s: 
+ * @s: char
+ *
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t caller[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (caller[i].op[0] == s[0])
+		{
+			return (caller[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- 0x0F-function_pointers/calc.h
+++ 0x0F-function_pointers/calc.h
@@ -1,6 +1,20 @@
 #ifndef CALC_H
 #define CALC_H
 
+/**
+ * struct op - structure def
+ * @op: char
+ * @f: function pointer
+ * @a: int
+ * @b: int
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
