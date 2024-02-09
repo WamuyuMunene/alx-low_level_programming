f82f26bb3a859851949e93f2bd422ec491090b64
WamuyuMunene <wamuyumunene98@gmail.com>
2024-02-09 04:15:07 -0800

    convert to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,11 +14,11 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int value = 0;
 	while (*b != '\0')
 	{
-		if (*b == 1)
+		if (*b == '1')
 		{
 			value = (value << 1) | 1;
 		}
-		else if (*b == 0)
+		else if (*b == '0')
 		{
 			value = value << 1;
 		}
