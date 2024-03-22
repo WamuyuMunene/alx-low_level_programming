9f6abe2b34ea68926af78b48262355850bda6162
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-22 07:47:46 -0700

    insert node at specific index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * *insert_dnodeint_at_index - inserts a node at specified index
+ *
+ * @@h: pointer
+ * @idx: int
+ * @n: int
+ *
+ * Return: address or NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
